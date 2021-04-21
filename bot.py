9ccacce2116d907aed69d8bd0b0ad6b00b34d333
Sagar <53867930+the-e3n@users.noreply.github.com>
2021-04-21 23:32:29 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# pylint: disable=C0116
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to send timed Telegram messages.
+This Bot uses the Updater class to handle the bot and the JobQueue to send
+timed messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic DOGE CryptoCurrency Price tracker/alarm, checks the price of api at a specific interval, defined by update interval.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+from time import sleep
+import os
+import logging
+from api import update
+drop_price = 20.00
+update_interval = 5
+
+TOKEN = os.getenv('BOT_TOKEN') # Enviroment variable named 'BOT_TOKEN'
+
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text('Hi! Use /set <price> to set a timer, Input price in INR to watch for. Replace <price> with INR')
+
+
+def alarm(context: CallbackContext) -> None:
+    """Send the alarm message."""
+    job = context.job
+    new_price = update()
+    if new_price<drop_price:
+      context.bot.send_message(job.context, text=f'Price Dropped!! New Price: Rs{new_price}')
+
+
+
+def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+def set_timer(update: Update, context: CallbackContext) -> None:
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        global drop_price
+        drop_price = float(context.args[0])
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_repeating(alarm,update_interval,context=chat_id, name=str(chat_id))
+        text = 'Timer successfully set!'
+        if job_removed:
+            text += ' Old one was removed.'
+        update.message.reply_text(text)
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /set <price> to watch for')
+
+
+def unset(update: Update, context: CallbackContext) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Timer successfully cancelled!' if job_removed else 'You have no active timer.'
+    update.message.reply_text(text)
+
+
+def main() -> None:
+    """Run bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(TOKEN)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", start))
+    dispatcher.add_handler(CommandHandler("set", set_timer))
+    dispatcher.add_handler(CommandHandler("unset", unset))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
+    # SIGABRT. This should be used most of the time, since start_polling() is
+    # non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
